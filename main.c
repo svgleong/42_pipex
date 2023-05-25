00f3e17fcd93628ce7e75430f7f809bd7ffa5ff9
Sofia Valente Goncalves Leong <svalente@c2r7s6.42lisboa.com>
2023-05-25 14:54:54 +0100

    refatoring

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: svalente <svalente@student.42lisboa.com >  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 14:57:14 by svalente          #+#    #+#             */
-/*   Updated: 2023/05/24 17:16:45 by svalente         ###   ########.fr       */
+/*   Updated: 2023/05/25 14:53:56 by svalente         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	main(int ac, char **av, char **envp)
 {
-	//int		fd1;
+	//int		fdd;
 	//int		fd2;
 	t_fds	fds;
 	
@@ -28,10 +28,15 @@ int	main(int ac, char **av, char **envp)
 	fds.fd1 = open(av[1], O_RDONLY);
 	fds.fd2 = open(av[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
 	if (fds.fd1 < 0 || fds.fd2 < 0)
-		perror("An error ocurred opening the files");
+		perror("An error ocurred opening the files"); //with p the xcvbn cat ls out works but the exit code is incorrect...
 	create_pipe(&fds, av, envp);
-	close(fds.fd1);
-	close(fds.fd2);
+	//close(fds.fd1);
+	//close(fds.fd2);
+	/* for (int i = 0; i < 30; i++)
+	{
+		fdd = open(av[1], O_RDONLY);
+		printf("fdd number: %d\n", fdd);
+	} */
 }
 
 void	create_pipe(t_fds *fds, char **av, char **envp)
@@ -46,13 +51,15 @@ void	create_pipe(t_fds *fds, char **av, char **envp)
 	pid_1 = fork();
 	if (pid_1 == -1)
 		error("An error occuring while forking"); 
-	if (pid_1 == 0 && fds->fd1 >= 0)
+	if (pid_1 == 0) {
+		
+	} // && fds->fd1 >= 0)
 		child_process1(fds, pipe_end, av[2], envp);
-	waitpid(pid_1, NULL, 0);
+	//waitpid(pid_1, NULL, 0); //with waitpid /dev/random doesn't work
 	pid_2 = fork();
 	if (pid_2 == -1)
 		error("An error occuring while forking\n");
-	if (pid_2 == 0 && fds->fd2 >= 0)
+	if (pid_2 == 0) // && fds->fd2 >= 0)
 		child_process2(fds, pipe_end, av[3], envp);
 	close_fds(pipe_end[0], pipe_end[1]);
 	waitpid(pid_2, NULL, 0);
